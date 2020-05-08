dc2d3c6a01e57bba81bd9a72c5cdd58a8578cb34
Aaron Osteraas <aaron.osteraas@gmail.com>
2020-05-08 22:01:24 +1000

    add react-redux

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -8,3 +8,6 @@ const rootReducer = combineReducers({
 export const store = configureStore({
   reducer: rootReducer,
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
