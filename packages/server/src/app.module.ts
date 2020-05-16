f2d822d989d1629603cf0ba71f5d266511d8958b
Aaron Osteraas <aaron.osteraas@gmail.com>
2020-05-16 10:43:19 +1000

    Add config to env, ignore, create example file

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from 'config/typeOrmConfig';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { QuizService } from './quiz/quiz.service';
 import { QuizModule } from './quiz/quiz.module';
 import { QuestionModule } from './question/question.module';
-
+ConfigModule.forRoot;
 @Module({
-  imports: [TypeOrmModule.forRootAsync(typeOrmConfig), AuthModule, QuizModule, QuestionModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres' as 'postgres',
+        host: configService.get('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 5432),
+        username: configService.get('DATABASE_USER', 'postgres'),
+        password: configService.get('DATABASE_PASS', 'postgres'),
+        database: configService.get('DATABASE_NAME', 'postgres'),
+        entities: [__dirname + '/**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+    QuizModule,
+    QuestionModule,
+  ],
   controllers: [],
   providers: [QuizService],
 })
