2ba86aba97aeb8932509c8833efdcfef000047d2
Aaron Osteraas <aaron.osteraas@gmail.com>
2020-05-19 19:09:37 +1000

    Add app route and navigation data

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Home, Login, NotFound, Register, Unauthorized } from '../pages';
 import { Layout } from 'antd';
 import { Navigation } from './Navigation';
-import { AppRoutes } from '../Routes';
+import { AppRouteData } from '../Routes';
 
 const App = () => {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path={AppRoutes.Home} element={<Home />} />
-        <Route path={AppRoutes.Login} element={<Login />} />
-        <Route path={AppRoutes.Register} element={<Register />} />
-        <Route path={AppRoutes.NotFound} element={<NotFound />} />
-        <Route path={AppRoutes.Unauthorized} element={<Unauthorized />} />
+        {AppRouteData.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
       </Routes>
       <Layout.Footer>
         <p>Footer</p>
--- packages/client/src/components/Navigation.tsx
+++ packages/client/src/components/Navigation.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
-import { AppRoutes } from '../Routes';
-
-const nav = [
-  { path: AppRoutes.Home, copy: 'Home' },
-  { path: AppRoutes.Login, copy: 'Login' },
-  { path: AppRoutes.Register, copy: 'Register' },
-];
+import { AppNavigation } from '../Routes';
 
 export const Navigation = () => {
   const location = useLocation();
@@ -19,7 +13,7 @@ export const Navigation = () => {
         mode="horizontal"
         defaultSelectedKeys={[location.pathname]}
       >
-        {nav.map(({ path, copy }, idx) => (
+        {AppNavigation.map(({ path, copy }) => (
           <Menu.Item key={path}>
             <Link to={path}>{copy}</Link>
           </Menu.Item>
