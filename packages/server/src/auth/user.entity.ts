c185905f0ef653f45813c978e6e4cffc8aa40ad8
David <davidsatwork2@hotmail.com>
2020-05-23 13:40:58 +0800

    add user.entity

---
+++ packages/server/src/auth/user.entity.ts
@@ -0,0 +1,20 @@
+import {Entity, BaseEntity, Unique, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+@Unique(['username', 'email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+}
