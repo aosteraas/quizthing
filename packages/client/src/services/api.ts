77bd6b83e0859be58797d041313e19da6210e1b0
Aaron Osteraas <aaron.osteraas@gmail.com>
2020-06-06 10:07:49 +1000

    Add http status codes enum for codes we'll probably end up ahndling

--- packages/client/src/services/api.ts
+++ packages/client/src/services/api.ts
@@ -10,6 +10,20 @@ export interface Api {
   setAuthToken: (shit: Nullable<string>) => void;
 }
 
+enum HttpStatus {
+  Ok = 200,
+  Created = 201,
+  NoContent = 204,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  Conflict = 409,
+  InternalServerError = 500,
+  BadGateway = 502,
+  ServiceUnavailable = 503,
+}
+
 const defaultHeaders = {
   'Content-Type': 'application/json',
 };
