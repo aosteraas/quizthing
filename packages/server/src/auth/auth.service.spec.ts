0d90b3be33dea844e2e2d7342d48919afa3d1d7c
Aaron Osteraas <aaron.osteraas@gmail.com>
2020-06-21 17:27:35 +1000

    add some starter tests

--- packages/server/src/auth/auth.service.spec.ts
+++ packages/server/src/auth/auth.service.spec.ts
@@ -2,17 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { JwtService } from '@nestjs/jwt';
+import { ConflictException } from '@nestjs/common';
 
 const mockUserRepository = () => ({
-  getTasks: jest.fn(),
-  findOne: jest.fn(),
-  createTask: jest.fn(),
+  signUp: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
   delete: jest.fn(),
 });
+
 const mockJwtService = () => ({});
+
 describe('AuthService', () => {
   let service: AuthService;
-  let repo: UserRepository;
+  let repo;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -35,5 +38,33 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  describe('signUp', () => {
+    const authCredDto = {
+      username: 'fakeuser',
+      email: 'fake@email.com',
+      password: 'fakepassword',
+    };
+    let save;
+    beforeEach(() => {
+      save = jest.fn();
+      repo.create = jest.fn().mockReturnValue({ save });
+    });
+    test('Creates a user', async () => {
+      await service.signUp(authCredDto);
+
+      expect(repo.signUp).toHaveBeenCalled();
+      expect(repo.signUp).toHaveBeenCalledWith(authCredDto);
+    });
+    test('Throws a conoflict exception when username or email exists', async () => {
+      save.mockRejectedValue({ code: '23505' });
+      expect(service.signUp(authCredDto)).rejects.toThrow(ConflictException);
+    });
+  });
+  describe('signIn', () => {
+    const loginDto = {
+      user: 'fakeuser',
+      password: 'fakepassword',
+    };
   });
 });
