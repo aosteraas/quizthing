d0425935c0d2451e1ac298045e404bf8f6d7515a
Aaron Osteraas <aaron.osteraas@gmail.com>
2020-06-24 19:33:00 +1000

    add description to quiz entity and creator dto, remove user from response

--- packages/server/src/quiz/quiz.repository.ts
+++ packages/server/src/quiz/quiz.repository.ts
@@ -8,7 +8,9 @@ export class QuizRepository extends Repository<Quiz> {
   async createQuiz(createQuizDto: CreateQuizDto, user: User) {
     const quiz = new Quiz();
     quiz.title = createQuizDto.title;
+    quiz.description = createQuizDto.description ?? '';
     quiz.user = user;
+
     await quiz.save();
 
     delete quiz.user;
